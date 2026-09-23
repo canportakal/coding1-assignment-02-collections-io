22bb4a09fc99099b5efd6c42ff46cf7ab78e374f
Can Portakal <canportakal35@gmail.com>
2026-09-23 17:16:40 +0200

    Added comments for every python code line

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
-drink = "tea"
-sales = {"tea": 3, "coffee": 5}
-sold = sales[drink]
-sold = sold + 1
-print(drink)
-print(sold)
+drink = "tea" # 'tea' is a string and it is labelled and stored in computer as a variable called 'drink'.
+sales = {"tea": 3, "coffee": 5} # It is a dictionary that contains the sales of different drinks. The keys are the names of the drinks and the values are the number of sales for each drink.
+sold = sales[drink] # 'sold' is a variable that stores the number of sales for the drink specified by the 'drink' variable.
+sold = sold + 1 # Increment the sales count for the specified drink. It will print 4.   
+print(drink) #It will print "tea" because the value of the variable 'drink' is 'tea'.
+print(sold) # It will print 4 because the sales count for 'tea' was incremented by 1 from its original value of 3.
